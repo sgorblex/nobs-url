c30f618338ba948a1c2c9384060f7a6dd3761838
Alessandro Clerici <ale.clericil@gmail.com>
2022-05-31 13:21:57 +0200

    database fixes

--- lib/cleanups.go
+++ lib/cleanups.go
@@ -6,20 +6,20 @@ import (
 )
 
 var Cleanups = map[string]func(string) (string, bool){
-	"https://www.amazon.it/.*/dp/.*/.*": func(url string) (string, bool) {
+	"https?://(www\\.)?amazon.it/.*/dp/.*/.*": func(url string) (string, bool) {
 		re := regexp.MustCompile("https://www.amazon.it/.*/dp/(.*?)/.*")
 		code := re.FindStringSubmatch(url)[1]
 		return "https://www.amazon.it/dp/" + code, true
 	},
-	"https://open.spotify.com/track/.*": func(url string) (string, bool) {
+	"https?://open\\.spotify\\.com/(track|playlist|album)/.*": func(url string) (string, bool) {
 		return rmAllGet(url), true
 	},
-	"https://www.reddit.com/r/.*/comments/.*/": func(url string) (string, bool) {
+	"https?://(www\\.)?reddit\\.com/r/.*/comments/.*/": func(url string) (string, bool) {
 		var re *regexp.Regexp
 		if strings.Contains(url, "/comment/") {
-			re = regexp.MustCompile("https://www.reddit.com/r/.*?/comments/.*/comment/.*?/")
+			re = regexp.MustCompile("https?://(www.)?reddit.com/r/.*?/comments/.*/comment/.*?/")
 		} else {
-			re = regexp.MustCompile("https://www.reddit.com/r/.*?/comments/.*?/")
+			re = regexp.MustCompile("https?://(www.)?reddit.com/r/.*?/comments/.*?/")
 		}
 		out := re.FindString(url)
 		return out[:len(out)-1], true
